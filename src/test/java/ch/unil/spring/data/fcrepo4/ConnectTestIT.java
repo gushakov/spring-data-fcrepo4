3d8ddb6c59f2a674efa4180f6bbecfad9fba6f77
George Ushakov <Georgy.Ushakov@unil.ch>
2015-04-27 17:18:53 +0200

    use assertj for tests

--- src/test/java/ch/unil/spring/data/fcrepo4/ConnectTestIT.java
+++ src/test/java/ch/unil/spring/data/fcrepo4/ConnectTestIT.java
@@ -1,17 +1,19 @@
 package ch.unil.spring.data.fcrepo4;
 
-import org.assertj.core.api.Assertions;
 import org.fcrepo.client.FedoraObject;
 import org.fcrepo.client.FedoraRepository;
-import org.fcrepo.client.impl.FedoraObjectImpl;
 import org.fcrepo.client.impl.ReadOnlyFedoraRepositoryImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static ch.unil.spring.data.fcrepo4.assertj.Assertions.assertThat;
+
 
 /**
  * @author gushakov
@@ -21,16 +23,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ConnectTestIT {
 
     @Configuration
+    @PropertySource("classpath:fcrepo4.properties")
     static class TestConfig {
 
     }
 
+    @Autowired
+    private Environment env;
+
     @Test
-    public void testConnect() throws Exception {
-       // read an existing object from Fedora
-        FedoraRepository repository = new ReadOnlyFedoraRepositoryImpl("http://localhost:9090");
-        FedoraObject fo = new FedoraObjectImpl(repository, null, "rest/ef/72/ee/79/ef72ee79-8a7f-4a8d-a4fb-1d1a63c26258");
-        assertThat(fo).isNotNull();
+    public void testReadObject() throws Exception {
+        // read an existing object from Fedora
+        String repoUrl = env.getProperty("fedora.repository.url");
+        // read-only access
+        FedoraRepository repository = new ReadOnlyFedoraRepositoryImpl(repoUrl);
+        FedoraObject fo = repository.findOrCreateObject("/test");
+        assertThat(fo)
+                .isNotNull()
+                .hasName("test")
+                .hasPath("/test")
+        ;
     }
 
 }
