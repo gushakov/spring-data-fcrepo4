20d8da36eccd6de74bf58cf5d5dc3b6f1386f3f8
George Ushakov <Georgy.Ushakov@unil.ch>
2015-09-25 08:24:42 +0200

    tx branch

---
+++ src/main/java/org/fcrepo/client/impl/TransactionalFedoraRepositoryImpl.java
@@ -0,0 +1,14 @@
+package org.fcrepo.client.impl;
+
+/**
+ * Adds methods to create, commit, and rollback transactions in Fedora repository. Normally, this will be incorporated
+ * into {@linkplain FedoraRepositoryImpl} at some point. Used here just for time being. Some code is copied from there.
+ *
+ * @author gushakov
+ */
+public class TransactionalFedoraRepositoryImpl extends FedoraRepositoryImpl {
+
+
+
+
+}
