c0115bb5ad1c312b7017ab68861be8956f69524f
George Ushakov <Georgy.Ushakov@unil.ch>
2016-02-17 08:23:22 +0100

    remove dependence on modeshape internal api

--- src/main/java/ch/unil/fcrepo4/spring/data/core/query/qom/Limit.java
+++ src/main/java/ch/unil/fcrepo4/spring/data/core/query/qom/Limit.java
@@ -1,7 +1,15 @@
 package ch.unil.fcrepo4.spring.data.core.query.qom;
 
 /**
+ * Copied from org.modeshape.jcr.api.query.qom.Limit
  * @author gushakov
  */
-public interface Limit extends org.modeshape.jcr.api.query.qom.Limit {
+public interface Limit {
+    int getOffset();
+
+    int getRowLimit();
+
+    boolean isUnlimited();
+
+    boolean isOffset();
 }
